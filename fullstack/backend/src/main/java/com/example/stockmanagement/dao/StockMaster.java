42e0672195a564eda73c10a51434ebfe38e7c043
Dhanush B H <107237163+DhanushShetty96@users.noreply.github.com>
2025-07-09 12:57:05 +0530

    added stock master dao

--- fullstack/backend/src/main/java/com/example/stockmanagement/dao/StockMaster.java
+++ fullstack/backend/src/main/java/com/example/stockmanagement/dao/StockMaster.java
@@ -1,5 +1,15 @@
 package com.example.stockmanagement.dao;
 
-public class StockMaster {
+import java.util.List;
+
+public interface StockMaster {
+	
+	public int InsertAndSendBackBId(StockMaster stock);
+	
+	public int getQunatityById(long bId);
+	
+	public boolean modifyStockQuantityByBId(long bId,int qunatity);
+	
+	public List<StockMaster> getAllStocksWithPostiveQuantity();
 
 }
